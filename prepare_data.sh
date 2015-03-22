b02e154166fc62372ad85459fb356357b2cc5be9
robertwenquan <robert.wen@nyu.edu>
2015-03-22 11:46:38 -0400

    modify prepare data script

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -fr UCI_HAR_Dataset &>/dev/null
+rm -fr "UCI HAR Dataset" &>/dev/null
 wget -q https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip -O UCI_HAR_Dataset.zip
 unzip -q UCI_HAR_Dataset.zip
-mv "UCI HAR Dataset" UCI_HAR_Dataset
+rm -f UCI_HAR_Dataset.zip
 
